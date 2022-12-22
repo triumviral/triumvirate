8b86e9978f942b64f1efc2f6c95e2549fe55f338
Triumvir <triumviral@pm.me>
2022-12-22 16:38:17 -0600

    conf configuration object config render options

--- src/main/scala/conf/Configuration.scala
+++ src/main/scala/conf/Configuration.scala
@@ -2,9 +2,10 @@ package conf
 
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.ClassTagExtensions
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigRenderOptions}
 
 object Configuration:
+  private val options: ConfigRenderOptions = ConfigRenderOptions.concise()
 
 class Configuration(private val hocon: Config,
                     private val json: JsonMapper & ClassTagExtensions,
