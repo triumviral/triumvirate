11ac7575c184561ceafb85f2ef2f4c657f2642f3
Triumvir <triumviral@pm.me>
2022-12-22 16:32:46 -0600

    conf configuration class

--- src/main/scala/conf/Configuration.scala
+++ src/main/scala/conf/Configuration.scala
@@ -1,3 +1,11 @@
 package conf
 
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.scala.ClassTagExtensions
+import com.typesafe.config.Config
+
 object Configuration:
+
+class Configuration(private val hocon: Config,
+                    private val json: JsonMapper & ClassTagExtensions,
+                   ):
