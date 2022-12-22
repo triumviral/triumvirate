7b823bed4afc465e41b025c5dd726eb57dd3ced6
Triumvir <triumviral@pm.me>
2022-12-22 16:40:20 -0600

    conf configuration object new configuration

--- src/main/scala/conf/Configuration.scala
+++ src/main/scala/conf/Configuration.scala
@@ -1,12 +1,20 @@
 package conf
 
 import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.module.scala.ClassTagExtensions
-import com.typesafe.config.{Config, ConfigRenderOptions}
+import com.fasterxml.jackson.module.scala.{ClassTagExtensions, DefaultScalaModule}
+import com.typesafe.config.{Config, ConfigFactory, ConfigRenderOptions}
 
 import scala.reflect.ClassTag
 
 object Configuration:
+  def apply(): Configuration = new Configuration(
+    hocon = ConfigFactory.defaultApplication(),
+    json = JsonMapper
+      .builder()
+      .addModule(DefaultScalaModule)
+      .build() :: ClassTagExtensions,
+  )
+
   private val options: ConfigRenderOptions = ConfigRenderOptions.concise()
 
 class Configuration(private val hocon: Config,
